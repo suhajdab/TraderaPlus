58a44dd764b6166c3dbd5afa4bc1d41713f3a00a
Balázs Suhajda <suhajdab@onereason.eu>
2011-11-04 00:05:01 +0100

    swedish language file

---
+++ js/sv.js
@@ -0,0 +1,9 @@
+var traderaPlusStrings = {
+		"about": "A Chrome Extention for tradera.com that will give you the following features:\n- block items ( stop looking at the same lame auction over & over because the title is interesting )\n- block users ( never see the cheap gadgets from the same user spamming your whole result page again )\n- add personal notes to auctions ( put a reminder about real price, special features, so you can quickly evaluate your max price )",
+		"addnote": "Add note",
+		"removenote": "Remove note",
+		"blockitem": "Block item",
+		"unblockitem": "Unblock item",
+		"blockuser": "Block user",
+		"unblockuser": "Unblock user"
+	};
