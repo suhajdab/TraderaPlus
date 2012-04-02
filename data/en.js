21706cb0fe9e31528f653cc048791917a111959b
Balázs Suhajda <balazs@onereason.eu>
2012-04-02 23:37:09 +0200

    data folder for js Strings

--- data/en.js
+++ data/en.js
@@ -1,11 +1,13 @@
 var traderaPlusStrings = {
 	"about": "A Chrome Extention for tradera.com that will give you the following features:\n- block items ( stop looking at the same lame auction over & over because the title is interesting )\n- block sellers ( never see the cheap gadgets from the same seller spamming your whole result page again )\n- add personal notes to auctions ( put a reminder about real price, special features, so you can quickly evaluate your max price )",
-	"addNote": "Lägg till anteckning",
+	"addNote": "Add note",
 	"notePlaceholder": "Add your personal, practical note here. Ex: new price, important qualities, web address to product",
-	"removeNote": "Radera anteckning",
-	"blockItem": "Blockera produkt",
-	"unblockItem": "Avblockera produkt",
-	"blockSeller": "Blockera säljare",
-	"unblockSeller": "Avblockera säljare",
-	"dropdownGuide": "To block any seller or item, or add a personal note, just click the little triangle available in the corner of all items."
+	"removeNote": "Remove note",
+	"blockItem": "Block item",
+	"unblockItem": "Unblock item",
+	"blockSeller": "Block seller",
+	"unblockSeller": "Unblock seller",
+	"showOptions": "Show options",
+	"dropdownGuide": "To block any seller or item, or add a personal note, just click the little triangle available in the corner of all items.",
+	"option_hideBlocked": "Hide blocked items"
 };
--- data/sv.js
+++ data/sv.js
@@ -1,11 +1,13 @@
 var traderaPlusStrings = {
 	"about": "A Chrome Extention for tradera.com that will give you the following features:\n- block items ( stop looking at the same lame auction over & over because the title is interesting )\n- block sellers ( never see the cheap gadgets from the same seller spamming your whole result page again )\n- add personal notes to auctions ( put a reminder about real price, special features, so you can quickly evaluate your max price )",
-	"addNote": "Add note",
+	"addNote": "Lägg till anteckning",
 	"notePlaceholder": "Add your personal, practical note here. Ex: new price, important qualities, web address to product",
-	"removeNote": "Remove note",
-	"blockItem": "Block item",
-	"unblockItem": "Unblock item",
-	"blockSeller": "Block seller",
-	"unblockSeller": "Unblock seller",
-	"dropdownGuide": "To block any seller or item, or add a personal note, just click the little triangle available in the corner of all items."
+	"removeNote": "Radera anteckning",
+	"blockItem": "Blockera produkt",
+	"unblockItem": "Avblockera produkt",
+	"blockSeller": "Blockera säljare",
+	"unblockSeller": "Avblockera säljare",
+	"showOptions": "Visa inställningarna",
+	"dropdownGuide": "To block any seller or item, or add a personal note, just click the little triangle available in the corner of all items.",
+	"option_hideBlocked": "Dölj alla blockerade"
 };
