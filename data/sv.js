8d7767486470fb1cc12580ca84510271d3561690
Balázs Suhajda <balazs@onereason.eu>
2014-07-30 17:43:37 +0200

    Translated missing swedish placeholder

--- data/sv.js
+++ data/sv.js
@@ -1,7 +1,7 @@
 var traderaPlusStrings = {
 	"about": "A Chrome Extention for tradera.com that will give you the following features:\n- block items ( stop looking at the same lame auction over & over because the title is interesting )\n- block sellers ( never see the cheap gadgets from the same seller spamming your whole result page again )\n- add personal notes to auctions ( put a reminder about real price, special features, so you can quickly evaluate your max price )",
 	"addNote": "Lägg till anteckning",
-	"notePlaceholder": "Add your personal, practical note here. Ex: new price, important qualities, web address to product",
+	"notePlaceholder": "Lägg din personliga, praktiska notera här. T.ex. nytt pris, viktiga egenskaper, webbadress till produkt, etc.",
 	"removeNote": "Radera anteckning",
 	"blockItem": "Blockera produkt",
 	"unblockItem": "Avblockera produkt",
